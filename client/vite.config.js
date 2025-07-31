571b32d794806073bfed3be9fbbda6f12033e12a
priyanshnihalani <priyansh.nihalani@gmail.com>
2025-07-31 15:42:19 +0530

    fix: allow Render domain in Vite preview config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import wasm from 'vite-plugin-wasm';
-import topLevelAwait from 'vite-plugin-top-level-await';
+import wasm from 'vite-plugin-wasm'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), wasm(), topLevelAwait()],
+  preview: {
+    host: true,
+    allowedHosts: ['sanchaya-webrtc-1.onrender.com'],
+  },
 })
