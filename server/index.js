06fc8fc0a3b11beb6c7c9ff97e50a6933976e7d7
priyanshnihalani <priyansh.nihalani@gmail.com>
2025-11-26 12:08:42 +0530

    Email Fixes

--- server/index.js
+++ server/index.js
@@ -135,7 +135,7 @@ app.post("/send-email", async (req, res) => {
 
     try {
         const transporter = nodemailer.createTransport({
-            service: "smtp.gmail.com",
+            host: "smtp.gmail.com",
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
