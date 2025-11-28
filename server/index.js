c072da831898e9492324ebce14ada80f3d0517cc
priyanshnihalani <priyansh.nihalani@gmail.com>
2025-11-28 12:23:13 +0530

    Mail Configuration changed

--- server/index.js
+++ server/index.js
@@ -26,6 +26,11 @@ app.use(cors({
 
 app.use(express.json())
 
+app.use((req, res, next) => {
+  console.log("➡️ Incoming:", req.method, req.path);
+  next();
+});
+
 const userSocketMap = new Map();
 const connection = new Map();
 
