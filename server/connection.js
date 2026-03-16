e118e3df1d7be86a498395a05d28fb0853526e61
Priyansh Nihalani <priyansh.nihalani@gmail.com>
2026-03-16 14:22:24 +0530

    connection changes

--- server/connection.js
+++ server/connection.js
@@ -1,31 +1,10 @@
 export function connectionSetUp(io) {
-
   io.on("connection", (socket) => {
-
     console.log("User connected:", socket.id);
 
     socket.on("join", (room) => {
       socket.join(room);
       console.log(`User ${socket.id} joined room ${room}`);
-    });
-
-    // receiver requests connection with sender
-    socket.on("connect-sender-receiver", ({ senderId, receiverId }) => {
-
-      io.to(senderId).emit("receiver-connection-request", {
-        receiverId
-      });
-
-    });
-
-    // sender approves receiver
-    socket.on("approve-receiver", ({ senderId, receiverId, approved }) => {
-
-      io.to(receiverId).emit(
-        approved ? "receiver-approved" : "receiver-rejected",
-        { senderId, approved }
-      );
-
     });
 
     socket.on("offer", ({ offer, room }) => {
@@ -43,7 +22,5 @@ export function connectionSetUp(io) {
     socket.on("disconnect", () => {
       console.log("User disconnected:", socket.id);
     });
-
   });
-
 }
